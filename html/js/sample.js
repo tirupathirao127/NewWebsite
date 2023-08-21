d8916e8f8079262322c8199d91bcff0769a16a2f
Tirupathi Rao Mandalapu <Tirupathi.Mandalapu@interflora.co.uk>
2023-08-21 18:01:37 +0530

    first js

---
+++ html/js/sample.js
@@ -0,0 +1,8 @@
+console.log("Hey")
+var num = 10;
+console.log(num)
+
+function display(num1) {
+    alert('Entered text ' + num1);
+    document.getElementById('alertmessage').innerHTML = "You entered " + num1;
+}
