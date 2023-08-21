1acc68a91346f44766e6c7f8d727f93adb8de409
Tirupathi Rao Mandalapu <Tirupathi.Mandalapu@interflora.co.uk>
2023-08-21 21:05:37 +0530

    added text color

--- html/js/sample.js
+++ html/js/sample.js
@@ -2,7 +2,18 @@ console.log("Hey")
 var num = 10;
 console.log(num)
 
+
+
 function display(num1) {
-    alert('Entered text ' + num1);
-    document.getElementById('alertmessage').innerHTML = "You entered " + num1;
+    console.log(num1)
+    if (num1.length > 0) {
+        alert('Entered text: ' + num1);
+        document.getElementById('alertmessage').innerHTML = "You entered: " + num1;
+        document.getElementById('alertmessage').style.color = "green";
+    } else {
+        alert('please enter text');
+        document.getElementById('alertmessage').innerHTML = "No text was entered! ";
+        document.getElementById('alertmessage').style.color = "red";
+    }
+    
 }
